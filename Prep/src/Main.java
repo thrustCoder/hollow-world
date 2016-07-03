24d792f303b3deac541e2f5d9ba57968d4f3dff6
thrustCoder <rajarshi129@gmail.com>
2016-07-02 21:55:35 -0700

    Commit from local IntelliJ

--- Prep/src/Main.java
+++ Prep/src/Main.java
@@ -1,8 +1,10 @@
 /**
  * Created by rpsin on 2/27/2016.
  */
-public class Main {
-    public static void main(String[] arg) {
-        System.out.println("Hollow world prep!!");
+public class Main
+{
+    public static void main(String[] arg)
+    {
+        System.out.println("Hollow world prep from local intelliJ!!");
     }
 }
