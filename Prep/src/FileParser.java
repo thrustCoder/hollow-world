40d71a04966d38faf364586127886bf70cfbefe5
thrustCoder <rajarshi129@gmail.com>
2016-07-03 12:43:30 -0700

    File parser program

---
+++ Prep/src/FileParser.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.IllegalFormatException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by rpsin on 7/3/2016.
+ */
+public class FileParser
+{
+    public FileParser()
+    {
+        mParseMap = new HashMap<>();
+    }
+
+    public String getElement(final String input)
+    {
+        if (input == null || input.equals(""))
+        {
+            throw new IllegalArgumentException("input should not be null or empty string");
+        }
+
+        if (mParseMap.containsKey(input))
+        {
+            return mParseMap.get(input);
+        }
+
+        return "NOT_FOUND";
+    }
+
+    public void parseFile(final String filePath) throws FileNotFoundException, IOException
+    {
+        if (filePath == null || filePath.equals(""))
+        {
+            throw new IllegalArgumentException("filePath should not be null or empty string");
+        }
+
+        if (mParseMap.size() > 0)
+        {
+            // reset parse map
+            mParseMap = new HashMap<>();
+        }
+
+        File file = new File(filePath);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line = br.readLine();
+
+        StringBuilder keyPrefix = new StringBuilder("");
+
+        while (line != null && !line.equals(""))
+        {
+            if (line.contains("[/"))
+            {
+                // end of tag
+                int startIndexForRemoval = (keyPrefix.lastIndexOf(".") > 0) ? keyPrefix.lastIndexOf(".") : 0;
+                keyPrefix.replace(startIndexForRemoval, (keyPrefix.length() - 1), "");
+            }
+            else if (line.contains("["))
+            {
+                // start of tag
+                if (keyPrefix.length() > 0)
+                {
+                    keyPrefix.append(".");
+                }
+                keyPrefix.append(line.substring(1, line.length() -1));
+            }
+            else
+            {
+                // key value pair
+                final String[] keyVal = line.split("=");
+                if ((keyVal.length < 2) || keyVal[0].equals(""))
+                {
+                    throw new IllegalArgumentException("Key value pair not formatted correctly");
+                }
+
+                final String key = keyPrefix + "." + keyVal[0];
+                mParseMap.put(key, keyVal[1]);
+            }
+
+            line = br.readLine();
+        }
+
+        br.close();
+    }
+
+    private Map<String, String> mParseMap;
+}
--- Prep/src/Main.java
+++ Prep/src/Main.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /**
  * Created by rpsin on 2/27/2016.
  */
@@ -6,5 +8,26 @@ public class Main
     public static void main(String[] arg)
     {
         System.out.println("Hollow world prep from local intelliJ!!");
+
+        parseFile();
+    }
+
+    private static final void parseFile()
+    {
+        // Q: https://www.careercup.com/question?id=5695752246394880
+        try
+        {
+            FileParser parser = new FileParser();
+            parser.parseFile("D:\\code\\github\\hollow-world\\Prep\\static\\FileParserFile");
+
+            System.out.println("employee.name: " + parser.getElement("employee.name"));
+            System.out.println("employee.address.houseno: " + parser.getElement("employee.address.houseno"));
+            System.out.println("employee.address.location.state: " + parser.getElement("employee.address.location.state"));
+            System.out.println("employee.manager: " + parser.getElement("employee.manager"));
+        }
+        catch (IOException e)
+        {
+            // do nothing here
+        }
     }
 }
