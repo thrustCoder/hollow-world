ca7c78eb6bda81decfb6148fc8f987010d9806f3
thrustCoder <rajarshi129@gmail.com>
2016-08-06 18:18:47 -0700

    merge resolve

--- Prep/src/Main.java
+++ Prep/src/Main.java
@@ -9,7 +9,27 @@ public class Main
     {
         System.out.println("Hollow world prep from local intelliJ!!");
 
-        parseFile();
+        swapBitiWithj();
+    }
+
+    private static void swapBitiWithj()
+    {
+        // Q: https://www.careercup.com/question?id=1941662
+        int i=0, j=2;
+        int a=4;
+        int ithBit = (a >> i) & 1;
+        int jthBit = (a >> j) & 1;
+
+        a = updateBit(a, j, ithBit);
+        a = updateBit(a, i, jthBit);
+
+        System.out.println(a); // should be 1
+    }
+
+    private static int updateBit(int num, int i, int v)
+    {
+        int mask = ~(1 << i);
+        return (num & mask) | (v << i);
     }
 
     private static final void parseFile()
