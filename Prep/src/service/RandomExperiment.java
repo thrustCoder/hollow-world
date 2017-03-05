dd8a3df5e5b9f737a12665bdd468a1ca8c54adb2
Singh <rajarshs@amazon.com>
2017-03-05 15:20:34 -0800

    Testing ZonedDateTime

--- Prep/src/service/RandomExperiment.java
+++ Prep/src/service/RandomExperiment.java
@@ -2,6 +2,7 @@ package service;
 
 import util.Printer;
 
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,14 +12,29 @@ import java.util.List;
  */
 public class RandomExperiment {
 
-    public static double randomExperiment() {
-        ZonedDateTime lipsDBBirthday = ZonedDateTime.parse("2017-02-24T00:00:00[America/Los_Angeles]");
-        ZonedDateTime toCompare = ZonedDateTime.parse("2017-02-25T00:00:00[America/Los_Angeles]");
+    public static void testZonedDateTime() {
+        ZonedDateTime lipsDBBirthday = ZonedDateTime.parse("2017-02-24T00:05:00Z[America/Los_Angeles]");
+        ZonedDateTime later = ZonedDateTime.parse("2017-02-25T00:00:00Z[America/Los_Angeles]");
+        ZonedDateTime before = ZonedDateTime.parse("2017-02-24T00:00:00Z[America/Los_Angeles]");
 
-        if (toCompare.toInstant()isBefore(lipsDBBirthday.toInstant())) {
-            Printer.print("Yess");
+        ZonedDateTime before2 = ZonedDateTime.parse("2017-02-24T00:10:00Z[Europe/Paris]");
+
+        if (later.toInstant().isAfter(lipsDBBirthday.toInstant())) {
+            Printer.println("Yes!");
         } else {
-            Printer.print("No");
+            Printer.println("??");
+        }
+
+        if (before.toInstant().isBefore(lipsDBBirthday.toInstant())) {
+            Printer.println("Yes!");
+        } else {
+            Printer.println("??");
+        }
+
+        if (before2.toInstant().isBefore(lipsDBBirthday.toInstant())) {
+            Printer.println("Yes!");
+        } else {
+            Printer.println("??");
         }
     }
 }
